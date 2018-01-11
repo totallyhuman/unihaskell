68e2c4756b9308534da9eb98e68df05fda6d20ad
Sumant Bhaskaruni <bsumantb@gmail.com>
2018-01-10 20:03:54 -0500

    Create unihaskell.hs

---
+++ unihaskell.hs
@@ -0,0 +1,90 @@
+{-# LANGUAGE UnicodeSyntax #-}
+
+import Data.List
+
+(∧)   ∷ Bool → Bool → Bool
+(∧)   = (&&)
+
+(∨)   ∷ Bool → Bool → Bool
+(∨)   = (||)
+
+(≡)   ∷ Eq a ⇒ a → a → Bool
+(≡)   = (==)
+
+(≢)   ∷ Eq a ⇒ a → a → Bool
+(≢)   = (/=)
+
+(≠)   ∷ Eq a ⇒ a → a → Bool
+(≠)   = (/=)
+
+(≤)   ∷ Ord a ⇒ a → a → Bool
+(≤)   = (<=)
+
+(≥)   ∷ Ord a ⇒ a → a → Bool
+(≥)   = (>=)
+
+(×)   ∷ Num a ⇒ a → a → a
+(×)   = (*)
+
+(÷)   ∷ Fractional a ⇒ a → a → a
+(÷)   = (/)
+
+(%)   ∷ Integral a ⇒ a → a → a
+(%)   = mod
+
+(∣)   ∷ Integral a ⇒ a → a → Bool
+a ∣ b = a % b == 0
+
+(∤)   ∷ Integral a ⇒ a → a → Bool
+(∤)   = (not .) . (∣)
+
+π     ∷ Floating a ⇒ a
+π     = pi
+
+(∘)   ∷ (b → c) → (a → b) → a → c
+(∘)   = (.)
+
+(⋅)   ∷ [a] → [a] → [a]
+(⋅)   = (++)
+
+(‼)   ∷ [a] → Int → a
+(‼)   = (!!)
+
+(∈)   ∷ (Foldable t, Eq a) ⇒ a → t a → Bool
+(∈)   = elem
+
+(∉)   ∷ (Foldable t, Eq a) ⇒ a → t a → Bool
+(∉)   = notElem
+
+(∋)   ∷ (Foldable t, Eq a) ⇒ t a → a → Bool
+(∋)   = flip elem
+
+(∌)   ∷ (Foldable t, Eq a) ⇒ t a → a → Bool
+(∌)   = flip notElem
+
+(∩)   ∷ Eq a ⇒ [a] → [a] → [a]
+(∩)   = intersect
+
+(∪)   ∷ Eq a ⇒ [a] → [a] → [a]
+(∪)   = union
+
+(∖)   ∷ Eq a ⇒ [a] → [a] → [a]
+(∖)   = (\\)
+
+(⊆)   ∷ Eq a ⇒ [a] → [a] → Bool
+(⊆)   = isInfixOf
+
+(⊇)   ∷ Eq a ⇒ [a] → [a] → Bool
+(⊇)   = flip isInfixOf
+
+(⊈)   ∷ Eq a ⇒ [a] → [a] → Bool
+(⊈)   = (not .) . isInfixOf
+
+(⊉)   ∷ Eq a ⇒ [a] → [a] → Bool
+(⊉)   = (not .) . flip isInfixOf
+
+(⊂)   ∷ Eq a ⇒ [a] → [a] → Bool
+a ⊂ b = (a ⊆ b) ∧ (a ≠ b)
+
+(⊃)   ∷ Eq a ⇒ [a] → [a] → Bool
+a ⊃ b = (a ⊇ b) ∧ (a ≠ b)
